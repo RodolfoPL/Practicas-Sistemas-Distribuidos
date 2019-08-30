7aebb3da60173d6e430f4e32daceeacda8293ae0
RodolfoPL <rodolpotter@hotmail.com>
2019-08-30 12:55:06 -0500

    practica 1 Relojes independientes

---
+++ Practica1/src/practica1/Practica1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+/**
+ *
+ * @author Lenovo
+ */
+public class Practica1 {
+    
+    public static void main(String[] args) {
+        RelojUI ui = new RelojUI();
+        
+        ui.mostrar();
+        
+    }
+    
+}
